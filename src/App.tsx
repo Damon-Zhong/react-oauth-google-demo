195954fb0e99ade35c384270d5948d7f73a120c6
Damon <damon@smart-one.ca>
2023-09-20 15:07:08 -0400

    demo set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+import type { CredentialResponse } from '@react-oauth/google'
 
 function App() {
+  const [loginUser, setLoginUser] = React.useState<CredentialResponse | null>(null)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GoogleOAuthProvider clientId='767787011140-e13l6211kju1nvqqe3m1dheap7vjf69s.apps.googleusercontent.com'>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>{loginUser ? `Welcome ${loginUser.credential}`:"Please login to continue"}</h1>
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <GoogleLogin
+            onSuccess={credentialResponse => {
+              console.log(credentialResponse);
+              setLoginUser(credentialResponse)
+            }}
+            onError={() => {
+              console.log('Login Failed');
+            }}
+          />
+        </header>
+      </div>
+    </GoogleOAuthProvider>
+
   );
 }
 
